535be49de77a3bbcbbf707efbf39aa87d92ddf3a
Tonmoy-Roy <emonismail69@gmail.com>
2023-01-26 21:53:15 +0600

    first commit

---
+++ no.1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int countSigDigits(string num)
+{
+    int count = 0;
+    bool trailingZeros = true;
+    bool hasDecimal = false;
+    for (int i = 0; i < num.length(); i++)
+    {
+        if (num[i] == '.')
+        {
+            hasDecimal = true;
+            continue;
+        }
+        if (num[i] == '0' && trailingZeros)
+        {
+            continue;
+        }
+        if (num[i] != '0')
+        {
+            trailingZeros = false;
+            count++;
+        }
+        if (hasDecimal && num[i] == '0' || !hasDecimal && num[i] == '0')
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main()
+{
+    string num1 = "0.000001234";
+    string num2 = "4500.000012";
+    cout << countSigDigits(num1) << endl;
+    cout << countSigDigits(num2) << endl;
+    return 0;
+}
